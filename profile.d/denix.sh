c1a6327e7be45c17e124d92ca2fc135a35db5d22
user <user@vm21.local>
2015-03-30 09:17:43 +0300

    Changes in profile script

--- profile.d/denix.sh
+++ profile.d/denix.sh
@@ -1,9 +1,11 @@
 if [ $UID = 0 ]; then
     export PS1='{\[\033[01;31m\]\u@`hostname`\[\033[00m\]}-> \[\033[01;33m\]\w\[\033[00m\] #'
     umask 022
+    user_color='\033[0;31m'
 else
     export PS1='{\[\033[01;32m\]\u@`hostname`\[\033[00m\]}-> \[\033[01;33m\]\w\[\033[00m\] $'
     umask 027
+    user_color='\033[0;32m'
 fi
 
 export HISTCONTROL=ignoreboth
@@ -33,12 +35,14 @@ __        __   _                            _
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-$color_Purple*------$color_Yellow System Data $color_Purple --------------------------------------------
+$color_Purple*------$color_Yellow System Data $color_Purple ---------------------------------------
 $color_Purple|$color_White Hostname              $color_Purple = $color_Cyan `hostname`
+$color_Purple|$color_White IP address/mask       $color_Purple = $color_Cyan `ip addr | grep 'state UP' -A2 | tail -1 | awk '{print $2}'`
 $color_Purple|$color_White Kernel                $color_Purple = $color_Cyan `uname -r`
 $color_Purple|$color_White Memory total          $color_Purple = $color_Cyan `cat /proc/meminfo | grep MemTotal | awk {'print $2'}` kB
 $color_Purple|$color_White Memory free           $color_Purple = $color_Cyan `cat /proc/meminfo | grep MemFree | awk {'print $2'}` kB
-$color_Purple|$color_White System uptime         $color_Purple = $color_Cyan `uptime | awk '{print $1}'`
-$color_Purple*----------------------------------------------------------------
+$color_Purple|$color_White System uptime         $color_Purple = $color_Cyan `uptime | awk -F',' '{print $1}' | awk -F'up ' '{print $2}'`
+$color_Purple|$color_White User                  $color_Purple = $user_color `whoami`
+$color_Purple*-----------------------------------------------------------
 $color_Off
 "
